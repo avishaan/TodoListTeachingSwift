201e272e292f41a89ddf52d7f3d8e9737de584ae
Avishaan <avishaan@codeHatcher.com>
2015-05-03 18:10:06 -0700

    SettingsViewController ViewDidLoad and change nav bar back button title and function called upon being pressed

--- TaskIt/SettingsViewController.swift
+++ TaskIt/SettingsViewController.swift
@@ -8,21 +8,48 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
   
   @IBOutlet weak var capitalizeTableView: UITableView!
   @IBOutlet weak var completeNewTodoTableView: UITableView!
   @IBOutlet weak var versionLabel: UILabel!
   
+  let kVersionNumber = "1.0"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Do any additional setup after loading the view.
+    
+    // add both delegate and datasource, alternatively you can do in storyboard
+    self.capitalizeTableView.delegate = self
+    self.capitalizeTableView.dataSource = self
+    // we don't want our user to scroll this table view since the number of rows will be fixed
+    self.capitalizeTableView.scrollEnabled = false
+    
+    self.completeNewTodoTableView.delegate = self
+    self.completeNewTodoTableView.dataSource = self
+    self.completeNewTodoTableView.scrollEnabled = false
+    
+    // update the title programmatically, alternatively could do in storyboard
+    self.title = "Settings"
+    
+    self.versionLabel.text = kVersionNumber
+    
+    // override default back button since we are using the show segue so we know how we can override it and make a different text
+    var doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItemStyle.Plain, target: self, action: Selector("doneBarButtonItemPressed:"))
+    // update left bar button item of our navigation
+    self.navigationItem.leftBarButtonItem = doneButton
+    
   }
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
+  }
+  
+  func doneBarButtonItemPressed (barButtonItem: UIBarButtonItem) {
+    self.navigationController?.popViewControllerAnimated(true)
   }
   
   
